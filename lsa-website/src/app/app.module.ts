524cee56eb84e814356bb9e7535f22bfa27ef837
elijahqm <eliasqm@msn.com>
2025-04-05 01:29:09 -0700

    Hide menus on small screens

--- lsa-website/src/app/app.module.ts
+++ lsa-website/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { inject, NgModule, provideAppInitializer } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatIconRegistry } from '@angular/material/icon';
 import { tap } from 'rxjs';
 import { AppConfigService } from './app-config.service';
 import { AppRoutingModule } from './app-routing.module';
@@ -22,4 +22,8 @@ export function initializeApp(configService: AppConfigService) {
     provideAppInitializer(() => initializeApp(inject(AppConfigService))),
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(iconRegistry: MatIconRegistry) {
+    iconRegistry.setDefaultFontSetClass('material-symbols-outlined');
+  }
+}
--- lsa-website/src/app/nav-bar/nav-bar.component.ts
+++ lsa-website/src/app/nav-bar/nav-bar.component.ts
@@ -1,11 +1,15 @@
-import { Component, effect, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { AppConfigService } from '../app-config.service';
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { ButtonConfig, NavigationOption } from '../models/app.config.models';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'lsa-nav-bar',
@@ -15,23 +19,41 @@ import { RouterLink } from '@angular/router';
     MatButtonModule,
     MatMenuModule,
     RouterLink,
+    CommonModule,
   ],
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent {
   title = '';
-  link = '/';
+  homeLink = '/';
   button?: ButtonConfig;
   options: NavigationOption[] = [];
 
-  constructor(private configService: AppConfigService) {
+  readonly activeOption = signal<NavigationOption | null>(null);
+  readonly shouldHide = signal(false);
+
+  constructor(
+    private configService: AppConfigService,
+    private breakpointObserver: BreakpointObserver
+  ) {
+    breakpointObserver.observe('(max-width: 850px)').subscribe((state) => {
+      this.shouldHide.set(state.matches);
+    });
     effect(() => {
       const config = this.configService.appConfig()?.navigation;
       this.title = config?.title ?? '';
-      this.link = config?.link ?? '/';
+      this.homeLink = config?.link ?? '/';
       this.button = config?.button;
       this.options = config?.options ?? [];
     });
+  }
+
+  setActiveOption(option?: NavigationOption) {
+    if (!option) {
+      this.activeOption.set(null);
+      return;
+    }
+    this.activeOption.set(option);
   }
 }
