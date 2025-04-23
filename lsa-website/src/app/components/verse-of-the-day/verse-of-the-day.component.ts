e7438dad06f85c75b30d42f03d1d722c50563b5d
hugoqui <hugo_2555@hotmail.com>
2025-04-22 18:50:06 -0600

    verse json, empty component

---
+++ lsa-website/src/app/components/verse-of-the-day/verse-of-the-day.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lsa-verse-of-the-day',
+  imports: [],
+  templateUrl: './verse-of-the-day.component.html',
+  styleUrl: './verse-of-the-day.component.scss'
+})
+export class VerseOfTheDayComponent {
+
+}
