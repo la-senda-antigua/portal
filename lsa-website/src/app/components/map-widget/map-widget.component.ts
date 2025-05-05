7a15bf471b891838ad3d28c5afc36e480be62c00
hugoqui <hugo_2555@hotmail.com>
2025-05-05 17:57:51 -0600

    create component

---
+++ lsa-website/src/app/components/map-widget/map-widget.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lsa-map-widget',
+  imports: [],
+  templateUrl: './map-widget.component.html',
+  styleUrl: './map-widget.component.scss'
+})
+export class MapWidgetComponent {
+
+}
