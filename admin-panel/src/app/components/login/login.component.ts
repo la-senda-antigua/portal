8d5f8ea0010f76879374b6dac11c269a68c48e7a
hugoqui <hugo_2555@hotmail.com>
2025-06-04 16:48:18 -0600

    login component

--- admin-panel/src/app/components/login/login.component.ts
+++ admin-panel/src/app/components/login/login.component.ts
@@ -1,37 +1,15 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
-import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCard, MatCardContent, MatCardTitle } from '@angular/material/card';
-
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  imports: [
-    FormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCard, MatCardTitle, MatCardContent
-  ],
+  imports: [],
 
 })
 export class LoginComponent {
-  username = '';
-  password = '';
-
-  constructor(private authService: AuthService, private router: Router) {}
-
-  onLogin() {
-    if (this.username && this.password) {
-      this.authService.login();
-      this.router.navigateByUrl('/');
-    }
-  }
+    
 }
