f9bf65c8d602ff8f4c027e3798728bd2eaa357d1
hugoqui <hugo_2555@hotmail.com>
2025-06-19 20:11:39 -0600

    controller

---
+++ lsa-web-apis/Controllers/SermosController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+
+namespace lsa_web_apis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SermosController : ControllerBase
+    {
+    }
+}
