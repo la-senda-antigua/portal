49c690a76cda21e5a8fb89175f55d84440c3ad13
hugoqui <hugo_2555@hotmail.com>
2025-07-18 09:53:34 -0600

    use routes ithComponentInputBinding

--- lsa-website/src/app/app-routing.module.ts
+++ lsa-website/src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { provideRouter, RouterModule, Routes, withComponentInputBinding  } from '@angular/router';
 import { PageRendererComponent } from './components/page-renderer/page-renderer.component';
 
-const routes: Routes = [
-  { path: ':id', component: PageRendererComponent },
+const routes: Routes  = [
+  { path: ':pageName', component: PageRendererComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home' }, // Redirect any unknown paths to home
 ];
 
 @NgModule({
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+  ],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
