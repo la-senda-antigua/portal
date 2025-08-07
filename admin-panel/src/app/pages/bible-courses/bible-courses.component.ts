c60a830068a6ccb619da11a033a435b0c743d169
hugoqui <hugo_2555@hotmail.com>
2025-08-06 19:39:24 -0600

    bible courses - update

--- admin-panel/src/app/pages/bible-courses/bible-courses.component.ts
+++ admin-panel/src/app/pages/bible-courses/bible-courses.component.ts
@@ -1,134 +1,161 @@
-import {
-  Component,
-  OnInit,
-  AfterViewInit,
-  ViewChild,
-  TemplateRef,
-} from '@angular/core';
-import { Sermon } from '../../models/Sermon';
-import { VideoRecordingsService } from '../../services/video-recordings.service';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatIconModule } from '@angular/material/icon';
-import { CommonModule, DatePipe } from '@angular/common';
-import { MatDialog, MatDialogRef, MatDialogContent, MatDialogActions } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit, signal, viewChild } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
-import { MatProgressBar } from '@angular/material/progress-bar';
-import { EditVideoFormComponent } from '../../components/edit-video-form/edit-video-form.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DeleteConfirmationData } from '../../components/delete-confirmation/delete-confirmation.component';
+import {
+  EditVideoFormComponent,
+  VideoFormData,
+} from '../../components/edit-video-form/edit-video-form.component';
+import {
+  TableViewColumn,
+  TableViewComponent,
+  TableViewDataSource,
+  TableViewType,
+} from '../../components/table-view/table-view.component';
+import { SermonDto } from '../../models/Sermon';
+import { VideoRecordingsService } from '../../services/video-recordings.service';
 
 @Component({
   selector: 'app-bible-courses',
-  imports: [MatTableModule, MatPaginatorModule, MatIconModule, DatePipe, MatDialogContent, MatDialogActions, MatButtonModule, MatProgressBar, CommonModule],
+  imports: [TableViewComponent],
   templateUrl: './bible-courses.component.html',
-  styleUrl: './bible-courses.component.scss'
+  styleUrl: './bible-courses.component.scss',
+  providers: [DatePipe],
 })
-export class BibleCoursesComponent implements OnInit, AfterViewInit {
-  displayedColumns: string[] = ['sermonId', 'title', 'preacher', 'date', 'actions'];
-  dataSource = new MatTableDataSource<Sermon>([]);
-  totalItems = 0;
-  pageSize = 10;
-  currentPage = 1;
-  isLoading = false;
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild('confirmDeleteDialog') confirmDeleteDialog!: TemplateRef<any>;
-  dialogRef!: MatDialogRef<any>;
+export class BibleCoursesComponent implements OnInit {
+  readonly tableViewComponent = viewChild(TableViewComponent);
+  readonly tableViewTypes = TableViewType;
+  readonly tableCols: TableViewColumn[] = [
+    { displayName: 'Id', datasourceName: 'id' },
+    { displayName: 'Title', datasourceName: 'title' },
+    { displayName: 'Preacher', datasourceName: 'preacherName' },
+    { displayName: 'Date', datasourceName: 'date' },
+  ];
+  readonly isLoading = signal(true);
+  readonly dataSource = signal<TableViewDataSource>({
+    totalItems: 0,
+    page: 1,
+    pageSize: 10,
+    items: [],
+    columns: this.tableCols,
+  });
+  readonly tableTitle = 'Bible Courses';
+  readonly editForm = EditVideoFormComponent;
+  readonly deleteFields: DeleteConfirmationData = {
+    id: 'id',
+    matchingString: 'title',
+    name: 'title',
+  };
 
   constructor(
-    private sermonsService: VideoRecordingsService,
-    private dialog: MatDialog
+    private videoRecordings: VideoRecordingsService,
+    private datePipe: DatePipe,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
-    this.loadCourses(this.currentPage, this.pageSize);
+    this.loadCourses(1, 10);
   }
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-  }
-
-  loadCourses(currentPage: number = 1, pageSize: number = 10): void {
-    this.isLoading = true;
-    this.sermonsService.getCourses(currentPage, pageSize)
-      .subscribe({
-        next: (response) => {
-          this.dataSource.data = response.items;
-          this.totalItems = response.totalItems;
-          this.pageSize = response.pageSize;
-          this.currentPage = response.page;
-          this.isLoading = false;
-        },
-        error: (err) => {
-          console.error('Error loading', err);
-          this.isLoading = false;
-        }
-      });      
+  loadCourses(currentPage: number, pageSize: number): void {
+    this.isLoading.set(true);
+    this.videoRecordings.getCourses(currentPage, pageSize).subscribe({
+      next: (response) => {
+        const courses = response.items.map((s) => ({
+          id: s.id,
+          date: this.datePipe.transform(s.date, 'yyyy-MM-dd'),
+          title: s.title,
+          preacherName: s.preacher.name,
+          preacherId: s.preacher.id,
+          cover: s.cover,
+          videoUrl: s.videoPath,
+        }));
+        this.dataSource.set({
+          page: response.page,
+          pageSize: response.pageSize,
+          totalItems: response.totalItems,
+          items: courses,
+          columns: this.tableCols,
+        });
+        this.isLoading.set(false);
+      },
+      error: (err) => {
+        this.handleException(err, 'There was an error loading courses.');
+      },
+    });
   }
 
   onPageChange(event: PageEvent) {
-    this.pageSize = event.pageSize;
-    this.currentPage = event.pageIndex + 1;
-    this.loadCourses(this.currentPage, this.pageSize);
+    this.loadCourses(event.pageIndex + 1, event.pageSize);
   }
 
-  async onDelete(course: Sermon) {
-    this.dialogRef = this.dialog.open(this.confirmDeleteDialog, {
-      data: course,
-    });
-
-    this.dialogRef.afterClosed().subscribe({
-      next: (confimed) => {
-        if (confimed) {
-          this.isLoading = true
-          this.sermonsService.deleteCourse(course.id);
-        }
+  async onDelete(id: string) {
+    this.isLoading.set(true);
+    this.videoRecordings.deleteCourse(parseInt(id)).subscribe({
+      next: () => {
+        this.reloadCourses();
       },
-      error: (err) =>{
-        this.isLoading = false
-        console.error('Error on deleting', err);
-      }
+      error: (err) => {
+        this.handleException(
+          err,
+          'There was a problem when attempting to delete this course.'
+        );
+      },
     });
   }
 
-  onEdit(course: Sermon) {
-    const dialogRef = this.dialog.open(EditVideoFormComponent, {
-      data: course,
-    });
-
-    dialogRef.afterClosed().subscribe((updatedCourse) => {
-      if (updatedCourse) {
-        this.isLoading = true
-        updatedCourse.id = course.id;
-        this.sermonsService.updateCourse(updatedCourse).subscribe({
-          next: () => {
-            this.loadCourses()
-          },
-          error: (err) => {
-            this.isLoading = false
-            console.error('Error on update', err);
-          },
-        });
-      }
+  onEdit(sermonForm: VideoFormData) {
+    this.isLoading.set(true);
+    const course = this.parseVideoForm(sermonForm) as any;
+    this.videoRecordings.updateCourse(course).subscribe({
+      next: () => {
+        this.reloadCourses();
+      },
+      error: (err) => {
+        this.handleException(err, 'There was a problem updating the course.');
+      },
     });
   }
 
-  onAdd() {
-    const dialogRef = this.dialog.open(EditVideoFormComponent);
-
-    dialogRef.afterClosed().subscribe((newCourse) => {
-      if (newCourse) {
-        this.isLoading = true
-        this.sermonsService.addCourse(newCourse).subscribe({
-          next: () => {
-            this.loadCourses()
-          },
-          error: (err) => {
-            this.isLoading = false
-            console.error('Error on add', err);
-          },
-        });
-      }
+  onAdd(sermonForm: VideoFormData) {
+    this.isLoading.set(true);
+    const course = this.parseVideoForm(sermonForm);
+    this.videoRecordings.addCourse(course as any).subscribe({
+      next: () => {
+        this.reloadCourses();
+      },
+      error: (err) => {
+        this.handleException(err, 'There was a problem adding the course.');
+      },
     });
+  }
+
+  private parseVideoForm(videoForm: VideoFormData): SermonDto {
+    const sermon = {
+      date: videoForm.data.date.toISOString().substring(0, 10),
+      title: videoForm.data.title,
+      videoPath: videoForm.data.videoUrl,
+      cover: videoForm.data.cover,
+      preacherId: videoForm.data.preacherId!,
+    } as SermonDto;
+    if (videoForm.data.id != undefined) {
+      sermon['id'] = videoForm.data.id;
+    }
+
+    return sermon;
+  }
+
+  private handleException(e: Error, message: string) {
+    this.isLoading.set(false);
+    console.error(e);
+    this.snackBar.open(message, '', {
+      duration: 4000,
+    });
+  }
+
+  private reloadCourses() {
+    const { pageSize, pageIndex } = this.tableViewComponent()!.paginator()!;
+    this.loadCourses(pageIndex + 1, pageSize);
   }
 }
