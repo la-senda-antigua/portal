bd86de531e8690d83eb7017a5efcf88e20e98847
hugoqui <hugo_2555@hotmail.com>
2025-09-08 10:41:47 -0600

    generate component vertial list

---
+++ lsa-website/src/app/components/vide-vertical-list/vide-vertical-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lsa-vide-vertical-list',
+  imports: [],
+  templateUrl: './vide-vertical-list.component.html',
+  styleUrl: './vide-vertical-list.component.scss'
+})
+export class VideVerticalListComponent {
+
+}
