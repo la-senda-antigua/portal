edd72b7f3ded1575703a394ad32eb126a2a12c34
hugoqui <hugo_2555@hotmail.com>
2025-10-22 11:30:14 -0600

    crud users

--- lsa-web-apis/Models/UserDto.cs
+++ lsa-web-apis/Models/UserDto.cs
@@ -2,5 +2,7 @@ namespace lsa_web_apis.Models;
 
 public class UserDto
 {
+    public Guid? Id { get; set; }
     public string Username { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
 }
