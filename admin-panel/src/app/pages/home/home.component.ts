e1d237267f83ce8fcae0b518044d41943e276555
hugoqui <hugo_2555@hotmail.com>
2025-12-04 21:53:50 -0600

    remove unused import

--- admin-panel/src/app/pages/home/home.component.ts
+++ admin-panel/src/app/pages/home/home.component.ts
@@ -3,11 +3,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { LogoutComponent } from "../../components/logout/logout.component";
 import { AuthService } from '../../services/auth.service';
-import { DateTimePickerComponent } from "../../components/date-time-picker/date-time-picker.component";
 
 @Component({
   selector: 'app-home',
-  imports: [MatIconModule, MatButtonModule, LogoutComponent, DateTimePickerComponent],
+  imports: [MatIconModule, MatButtonModule, LogoutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
