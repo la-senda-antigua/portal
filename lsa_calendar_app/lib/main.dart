33f3c3b1d074487c3dda48ed157bd348754b9699
hugo <hugo2555@gmail.com>
2026-01-06 20:14:48 -0600

    clean up - unused references

--- lsa_calendar_app/lib/main.dart
+++ lsa_calendar_app/lib/main.dart
@@ -1,4 +1,3 @@
-// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:lsa_calendar_app/core/app_colors.dart';
@@ -7,8 +6,7 @@ import 'package:lsa_calendar_app/screens/login_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Leemos la variable 'ENV_FILE' desde la compilación.
-  // Si no se especifica nada, usará '.env' por defecto.
+  // use '.env' by default
   const envFile = String.fromEnvironment('ENV_FILE', defaultValue: '.env');
   await dotenv.load(fileName: envFile);
 
